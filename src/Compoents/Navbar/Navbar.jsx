0bd46b8e4d8843e05453664f633a2eb38035b70b
Rodent12 <jainsayyam1234@gmail.com>
2024-10-27 18:12:04 +0530

    changed Navbar

--- src/Compoents/Navbar/Navbar.jsx
+++ src/Compoents/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Navbar.css'
-import nav_logo from '../../assets/nav-logo.svg'
+import logo from '../../assets/logo.png'
 import nav_profile from '../../assets/nav-profile.svg'
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={nav_logo} alt="" className="nav-logo" />
+        <div className='nav-logo'>
+            <img src={logo} alt=""/>
+            <p>KhareedoIndia</p>
+        </div>
         <img src={nav_profile} alt="" className="nav-profile" />
     </div>
   )
